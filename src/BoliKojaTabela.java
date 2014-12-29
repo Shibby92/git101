27909f020761b44eadda671cd8df1900f8bc7394
Shibby92 <haris_000@hotmail.com>
2014-12-29 17:59:12 +0100

    Added space

--- src/BoliKojaTabela.java
+++ src/BoliKojaTabela.java
@@ -15,6 +15,8 @@ public class BoliKojaTabela {
 
 	private static void odigrajPotez(char[][] igra, char znak, String pozicija) {
 		int kolona= pozicija.charAt(0)-'A';
+		
+		
 		int red= Integer.parseInt(pozicija.substring(1));
 		igra[red][kolona]=znak;
 		ispisiStanjeIgre(igra);
